e07230b9202a38fe76f60f0c7374570ee55bc21a
Andres Ottonello <andres.ottonello@edessa.com.ar>
2021-07-30 17:11:59 -0300

    paneles simples

--- src/components/ContentRowProperties.js
+++ src/components/ContentRowProperties.js
@@ -7,31 +7,46 @@ class ContentRowProperties extends Component {
     constructor(){
         super()
         this.state ={
-            cardProps : [],
+            cardData : [],
         }
     }
 
-    componentDidMount(){
-        fetch('http://localhost:3002/api/properties')
-        .then(respuesta =>{
-            return respuesta.json()
-        })
-        .then(properties =>{
-            //console.log(genres)
-            this.setState({  cardProps : [
-                {
-                    color:   "primary",
-                    titulo: "Propeties in Data Base",
-                    valor: properties.meta.count,
-                    icono: "fas fa-film",                    
-                }
-            ]  })
-        })
-        .catch(error => console.log(error));
+    async componentDidMount(){
+
+        let props = {
+            color:   "primary",
+            titulo: "Properties in Data Base",
+            valor: 0,
+            icono: "fas fa-home",                    
+        }
+        let usr = {
+            color:   "success",
+            titulo: "Users in Data Base",
+            valor: 0,
+            icono: "fas fa-users",
+        }
+        let cat = {
+            color:   "warning",
+            titulo: "Categories in Data Base",
+            valor: 0,
+            icono: "fas fa-address-card",
+        }
+
+        //Obtengo el total de propiedades en BD's
+        let properties = await (await fetch('http://localhost:3002/api/properties')).json();
+        if (properties) {
+            props['valor'] = properties.meta.count;
+            //Obtengo las categorias
+            let i=0;
+            for (const prop in properties.meta.countByCategory) i++;
+            cat['valor']=i;
+        }
+        //Obtengo el total de usuarios en BD's
+        let users = await (await fetch('http://localhost:3002/api/users')).json();
+        if (users) usr['valor'] = users.meta.count;
+
+        this.setState({  cardData : [ props, usr, cat ] });
     }
-
-
-
 
     render() {
         return (
@@ -39,8 +54,8 @@ class ContentRowProperties extends Component {
             {/* <!-- Content Row --> */}
             <div className="row">
                 {
-                    this.state.cardProps.map((producto,index)=>{
-                        return <SmallCard  {...producto}  key= {index}/>
+                    this.state.cardData.map((data,index)=>{
+                        return <SmallCard  {...data}  key= {index}/>
                     })
                 }      
             </div>
